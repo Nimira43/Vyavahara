76f7e856415b6d239f67e0f1fe8c0f22eb72328c
Nimira43 <n.rabone72@aol.com>
2024-12-28 21:58:48 +0000

    Added properties to Theme Provider.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,12 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={poppins.className}>
-        <ThemeProvider>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
           {children}
         </ThemeProvider>
       </body>
