5787cbe3ce7a0ac95af02a2a8dcfb15e57de9ead
Nimira43 <n.rabone72@aol.com>
2025-03-04 01:57:10 +0000

    Setup interface inside business.tsx

--- context/business.tsx
+++ context/business.tsx
@@ -1,3 +1,25 @@
 'use client'
 
 import { createContext, createServerContext, useState, ReactNode } from 'react'
+
+interface BusinessState {
+  _id: string
+  userEmail: string
+  name: string
+  category: string
+  description: string
+  address: string
+  phone: string
+  email: string
+  website: string
+  hours: string
+  logo: string
+  businessNumber: string
+  status: string
+  slug: string
+  published?: boolean
+  createdAt: string
+  updatedAt: string
+  __v: number
+
+}
