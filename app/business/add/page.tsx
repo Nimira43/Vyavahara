9b6a72e2a6028d8127a9edffb062399cb195939c
Nimira43 <n.rabone72@aol.com>
2025-03-04 03:07:17 +0000

    Using JSON.stringify to display on screen the current business state - working well.

--- app/business/add/page.tsx
+++ app/business/add/page.tsx
@@ -1,8 +1,18 @@
+'use client'
+import { useBusiness } from '@/context/business'
+
 export default function AddBusiness() {
+  const { business } = useBusiness()
+  
   return (
     <div className='flex flex-col lg:flex-row h-screen'>
       <div className='flex flex-col lg:w-1/2 p-4 lg:order-last lg:flex lg:justify-center lg:items-center'>Preview</div>
-      <div className='flex flex-col lg:w-1/2 p-4 lg:order-first lg:flex lg:justify-center lg:items-center'>Form</div>
+
+
+      <div className='flex flex-col lg:w-1/2 p-4 lg:order-first lg:flex lg:justify-center lg:items-center'>
+        Form
+        <pre>{JSON.stringify(business, null, 4)}</pre>
+      </div>
     </div>
   )
 }
