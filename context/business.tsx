243235663fd27ddf319204d6db461427fbf06dc9
Nimira43 <n.rabone72@aol.com>
2025-03-04 02:22:55 +0000

    Loading interface and state.

--- context/business.tsx
+++ context/business.tsx
@@ -45,6 +45,8 @@ const initialState: BusinessState = {
 interface BusinessContextType {
   business: BusinessState
   setBusiness: React.Dispatch<React.SetStateAction<BusinessState>>
+  loading: boolean
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const BusinessContext = createContext<BusinessContextType | undefined>(undefined)
@@ -53,6 +55,7 @@ export const BusinessProvider: React.FC<{ children: ReactNode }> = ({
   children
 }) => {
   const [business, setBusiness] = useState<BusinessState>(initialState)
+  const [loading, setLoading] = useState<boolean>(false)
   return (
     <BusinessContext.Provider
       value={{ business, setBusiness}}
