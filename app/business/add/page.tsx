837d4b8a9c438920151411443a0c7c0e35cfaf7d
Nimira43 <n.rabone72@aol.com>
2025-03-18 03:37:18 +0000

    Added required field

--- app/business/add/page.tsx
+++ app/business/add/page.tsx
@@ -5,19 +5,20 @@ interface InputField {
   name: string
   type: string
   label: string
-  placeholder: string
+  placeholder: string,
+  required: true
 }
 
 const inputFields: InputField = [
-  { name: 'name', type: 'text', label: 'Name', placeholder: 'Business Name'},
-  { name: 'category', type: 'text', label: 'Category', placeholder: 'Category'},
-  { name: 'description', type: 'text', label: 'Description', placeholder: 'Description'},
-  { name: 'address', type: 'text', label: 'Address', placeholder: 'Address'},
-  { name: 'phone', type: 'text', label: 'Phone', placeholder: 'Phone'},
-  { name: 'email', type: 'text', label: 'Email', placeholder: 'Email'},
-  { name: 'website', type: 'text', label: 'Website', placeholder: 'Website'},
-  { name: 'hours', type: 'text', label: 'Hours', placeholder: 'Hours'},
-  { name: 'logo', type: 'text', label: 'Logo', placeholder: 'Logo'}
+  { name: 'name', type: 'text', label: 'Name', placeholder: 'Business Name', required: true},
+  { name: 'category', type: 'text', label: 'Category', placeholder: 'Category', required: true},
+  { name: 'description', type: 'text', label: 'Description', placeholder: 'Description', required: true},
+  { name: 'address', type: 'text', label: 'Address', placeholder: 'Address', required: true},
+  { name: 'phone', type: 'text', label: 'Phone', placeholder: 'Phone', required: true},
+  { name: 'email', type: 'text', label: 'Email', placeholder: 'Email', required: true},
+  { name: 'website', type: 'text', label: 'Website', placeholder: 'Website', required: true},
+  { name: 'hours', type: 'text', label: 'Hours', placeholder: 'Hours', required: true},
+  { name: 'logo', type: 'text', label: 'Logo', placeholder: 'Logo', required: true}
 ]
 
 export default function AddBusiness() {
