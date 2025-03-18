eac4360d88aa8a49acc4adf93686f26b73be040d
Nimira43 <n.rabone72@aol.com>
2025-03-18 03:44:07 +0000

    Deleted placeholder field. Icluded optional accept field.

--- app/business/add/page.tsx
+++ app/business/add/page.tsx
@@ -5,20 +5,20 @@ interface InputField {
   name: string
   type: string
   label: string
-  placeholder: string,
   required: true
+  accept?: string
 }
 
 const inputFields: InputField = [
-  { name: 'name', type: 'text', label: 'Name', placeholder: 'Business Name', required: true},
-  { name: 'category', type: 'text', label: 'Category', placeholder: 'Category', required: true},
-  { name: 'description', type: 'text', label: 'Description', placeholder: 'Description', required: true},
-  { name: 'address', type: 'text', label: 'Address', placeholder: 'Address', required: true},
-  { name: 'phone', type: 'text', label: 'Phone', placeholder: 'Phone', required: true},
-  { name: 'email', type: 'text', label: 'Email', placeholder: 'Email', required: true},
-  { name: 'website', type: 'text', label: 'Website', placeholder: 'Website', required: true},
-  { name: 'hours', type: 'text', label: 'Hours', placeholder: 'Hours', required: true},
-  { name: 'logo', type: 'text', label: 'Logo', placeholder: 'Logo', required: true}
+  { name: 'name', type: 'text', label: 'Name', required: true},
+  { name: 'category', type: 'text', label: 'Category', required: true},
+  { name: 'description', type: 'text', label: 'Description', required: true},
+  { name: 'address', type: 'text', label: 'Address', required: true},
+  { name: 'phone', type: 'text', label: 'Phone', required: true},
+  { name: 'email', type: 'text', label: 'Email', required: true},
+  { name: 'website', type: 'text', label: 'Website', required: true},
+  { name: 'hours', type: 'text', label: 'Hours', required: true},
+  { name: 'logo', type: 'text', label: 'Logo', required: true}
 ]
 
 export default function AddBusiness() {
