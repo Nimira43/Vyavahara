ae2d86a62e1bd9fdb444722f2d24df5a79b4ee46
Nimira43 <n.rabone72@aol.com>
2025-05-05 00:04:24 +0100

    Importing card components

--- components/business/preview/preview-card.tsx
+++ components/business/preview/preview-card.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { BusinessState } from '@/utils/types/business'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function PreviewCard({ business }: { business: BusinessState}) {
   return (
