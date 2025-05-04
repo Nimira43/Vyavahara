41582e3aeeae8b809ee30675032cd529e074a40c
Nimira43 <n.rabone72@aol.com>
2025-05-05 00:08:15 +0100

    Mail icon

--- components/business/preview/preview-card.tsx
+++ components/business/preview/preview-card.tsx
@@ -4,6 +4,7 @@ import { BusinessState } from '@/utils/types/business'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { VscLocation } from "react-icons/vsc"
 import { TbPhone } from "react-icons/tb"
+import { AiOutlineMail } from "react-icons/ai"
 
 export default function PreviewCard({ business }: { business: BusinessState}) {
   return (
