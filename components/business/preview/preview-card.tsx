2a9fdfdf4066840c1b0d7ce5e3354419abc7a0be
Nimira43 <n.rabone72@aol.com>
2025-05-05 00:10:55 +0100

    Globe icon

--- components/business/preview/preview-card.tsx
+++ components/business/preview/preview-card.tsx
@@ -5,8 +5,10 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { VscLocation } from "react-icons/vsc"
 import { TbPhone } from "react-icons/tb"
 import { AiOutlineMail } from "react-icons/ai"
+import { PiGlobeStandLight } from "react-icons/pi"
 
-export default function PreviewCard({ business }: { business: BusinessState}) {
+
+export default function PreviewCard({ business }: { business: BusinessState }) {
   return (
     <div>
       
