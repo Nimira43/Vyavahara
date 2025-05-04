8a6da33bd17b54c0308667522cb80ca521846ac9
Nimira43 <n.rabone72@aol.com>
2025-05-05 00:07:23 +0100

    Importing location and phone icons

--- components/business/preview/preview-card.tsx
+++ components/business/preview/preview-card.tsx
@@ -2,6 +2,8 @@
 
 import { BusinessState } from '@/utils/types/business'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { VscLocation } from "react-icons/vsc"
+import { TbPhone } from "react-icons/tb"
 
 export default function PreviewCard({ business }: { business: BusinessState}) {
   return (
