7c08a00ab88a78ce0d57eca1b0e4c1634bb377d6
Nimira43 <n.rabone72@aol.com>
2025-06-23 22:42:04 +0100

    Completed BusinessSchema

--- models/business.ts
+++ models/business.ts
@@ -9,8 +9,31 @@ const BusinessSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-    }
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    address: String,
+    phone: String,
+    email: String,
+    website: String,
+    hours: String,
+    logo: String,
+    businessNumber: String,
+    slug: {
+      type: String,
+      required: true,
+      lowercase: true,
+      index: true,
+    },
+    published: {
+      type: Boolean,
+      default: true,
+    },
   },
   {
   timestamps: true
-  })
+  }
+)
