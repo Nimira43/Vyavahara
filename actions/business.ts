73ae3d084283bd3b4ecf57022bfa301373973570
Nimira43 <n.rabone72@aol.com>
2025-08-03 08:46:22 +0100

    Import nanoid

--- actions/business.ts
+++ actions/business.ts
@@ -4,4 +4,5 @@ import db from '@/utils/db'
 import Business from '@/models/business'
 import { currentUser } from '@clerk/nextjs/server'
 import { BusinessState } from '@/utils/types/business'
+import { nanoid } from 'nanoid'
 
