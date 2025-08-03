d574340b54d64ca71c037c397953ade08e53c6f6
Nimira43 <n.rabone72@aol.com>
2025-08-03 08:59:32 +0100

    Creating new server action method saveBusinessToDb

--- actions/business.ts
+++ actions/business.ts
@@ -6,3 +6,7 @@ import { currentUser } from '@clerk/nextjs/server'
 import { BusinessState } from '@/utils/types/business'
 import { nanoid } from 'nanoid'
 import slugify from 'slugify'
+
+export const saveBusinessToDb = async (data: BusinessState) => {
+  
+}
